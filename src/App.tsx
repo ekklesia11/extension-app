93f1588a17aa84b90d97845901feb9996af6006d
front_박찬현 <happyyquokka@cashwalk.io>
2021-08-18 08:53:32 +0900

    :tada:

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [url, setUrl] = useState<string>('');
+
+    /**
+     * Get current URL
+     */
+    useEffect(() => {
+        const queryInfo = {active: true, lastFocusedWindow: true};
+
+        if (typeof window === 'object' && window) {
+          const chrome: any = (window as any).chrome
+          chrome.tabs && chrome.tabs.query(queryInfo, (tabs: any) => {
+              const url = tabs[0].url;
+              setUrl(url);
+          });
+        }
+    }, []);
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>URL:</p>
+                <p>
+                    {url}
+                </p>
+            </header>
+        </div>
+    );
+};
 
 export default App;
